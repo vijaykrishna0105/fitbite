f3f8b64a72439a6cc2362597557ec69c8a7fddf0
Vijay Krishna <pamula19183@iiitd.ac.in>
2024-01-26 15:39:50 +0530

    Completed Version 1 on Frontend and Backend Registration Done

--- Frontend_Restaurant_FInal/src/components/About.jsx
+++ Frontend_Restaurant_FInal/src/components/About.jsx
@@ -12,9 +12,17 @@ const About = () => {
             <p>The only thing we're serious about is food.</p>
           </div>
           <p className="mid">
-          Welcome to FitBite, where every morsel is a step towards your fitness goals. Our philosophy is simple: energize your body with meals that are as delicious as they are nutritious. Designed with gym enthusiasts and fitness gurus in mind, FitBite delivers a menu that complements your active lifestyle. Our culinary experts craft every dish to fuel performance, promote muscle recovery, and ignite your metabolism. We source the freshest ingredients, emphasize lean proteins, whole grains, and powerhouse greens to create meals that satisfy your taste buds and support your wellbeing. At FitBite, we understand that time is of the essence, and convenience should not come at the expense of health
-            
+            Welcome to FitBite, where every morsel is a step towards your
+            fitness goals. Our philosophy is simple: energize your body with
+            meals that are as delicious as they are nutritious. Designed with
+            gym enthusiasts and fitness gurus in mind, FitBite delivers a menu
+            that complements your active lifestyle. Our culinary experts craft
+            every dish to fuel performance, promote muscle recovery, and ignite
+            your metabolism. We source the freshest ingredients, emphasize lean
+            proteins, whole grains, and powerhouse greens to create meals that
+            satisfy your taste buds and support your wellbeing.
           </p>
+          
           <Link to={"menu"} spy={true} smooth={true} duration={500}>
             Explore Menu{" "}
             <span>
--- Frontend_Restaurant_FInal/src/components/HeroSection.jsx
+++ Frontend_Restaurant_FInal/src/components/HeroSection.jsx
@@ -19,15 +19,15 @@ const HeroSection = () => {
               <div className="textWithSvg">
                 <h1 className="title">Food</h1>
                 <h1 className="title dishes_title">Dishes</h1>
-                { <img src="/threelines.svg" alt="three" />}
+                
               </div>
-              {/* <img src="/logo.svg" alt="logo" className="logo" /> */}
+              
             </div>
           </div>
         </div>
         <div className="banner">
           <div className="imageBox">
-            <img src="/493ss_Thinsktock_rf_banana_and_apples.webp" alt="hero2" />
+            <img src="https://images.unsplash.com/photo-1597579018905-8c807adfbed4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="hero2" />
           </div>
           <h1 className="title dishes_title">Dishes</h1>
         </div>
